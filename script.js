2870b309f7d80f451bc21624ed30730de2ed3b6e
Madeline MORISSET <madeline.morisset@outlook>
2023-05-22 15:00:54 +0200

    feature/board_design - Placement des cases sur le plateau

---
+++ script.js
@@ -0,0 +1,210 @@
+let plateau = [
+    {
+        numeroCase: 0,
+        positionX: 60,
+        positionY: 40,
+        bonus: false,
+        malus: false,
+        occupe: undefined
+    },
+    {
+        numeroCase: 1,
+        positionX: 140,
+        positionY: 130,
+        bonus: false,
+        malus: false,
+        occupe: undefined
+    },
+    {
+        numeroCase: 2,
+        positionX: 210,
+        positionY: 240,
+        bonus: false,
+        malus: false,
+        occupe: undefined
+    },
+    {
+        numeroCase: 3,
+        positionX: 170,
+        positionY: 350,
+        bonus: false,
+        malus: false,
+        occupe: undefined
+    },
+    {
+        numeroCase: 4,
+        positionX: 100,
+        positionY: 450,
+        bonus: false,
+        malus: false,
+        occupe: undefined
+    },
+    {
+        numeroCase: 5,
+        positionX: 150,
+        positionY: 560,
+        bonus: false,
+        malus: true,
+        occupe: undefined
+    },
+    {
+        numeroCase: 6,
+        positionX: 250,
+        positionY: 630,
+        bonus: false,
+        malus: false,
+        occupe: undefined
+    },
+    {
+        numeroCase: 7,
+        positionX: 350,
+        positionY: 560,
+        bonus: false,
+        malus: false,
+        occupe: undefined
+    },
+    {
+        numeroCase: 8,
+        positionX: 400,
+        positionY: 450,
+        bonus: false,
+        malus: false,
+        occupe: undefined
+    },
+    {
+        numeroCase: 9,
+        positionX: 400,
+        positionY: 330,
+        bonus: false,
+        malus: false,
+        occupe: undefined
+    },
+    {
+        numeroCase: 10,
+        positionX: 360,
+        positionY: 210,
+        bonus: true,
+        malus: false,
+        occupe: undefined
+    },
+    {
+        numeroCase: 11,
+        positionX: 380,
+        positionY: 90,
+        bonus: false,
+        malus: false,
+        occupe: undefined
+    },
+    {
+        numeroCase: 0,
+        positionX: 60,
+        positionY: 40,
+        bonus: false,
+        malus: false,
+        occupe: undefined
+    },
+    {
+        numeroCase: 0,
+        positionX: 60,
+        positionY: 40,
+        bonus: false,
+        malus: false,
+        occupe: undefined
+    },
+    {
+        numeroCase: 0,
+        positionX: 60,
+        positionY: 40,
+        bonus: false,
+        malus: false,
+        occupe: undefined
+    },
+    {
+        numeroCase: 0,
+        positionX: 60,
+        positionY: 40,
+        bonus: false,
+        malus: false,
+        occupe: undefined
+    },
+    {
+        numeroCase: 0,
+        positionX: 60,
+        positionY: 40,
+        bonus: false,
+        malus: false,
+        occupe: undefined
+    },
+    {
+        numeroCase: 0,
+        positionX: 60,
+        positionY: 40,
+        bonus: false,
+        malus: false,
+        occupe: undefined
+    },
+    {
+        numeroCase: 0,
+        positionX: 60,
+        positionY: 40,
+        bonus: false,
+        malus: false,
+        occupe: undefined
+    },
+    {
+        numeroCase: 0,
+        positionX: 60,
+        positionY: 40,
+        bonus: false,
+        malus: false,
+        occupe: undefined
+    },
+    {
+        numeroCase: 0,
+        positionX: 60,
+        positionY: 40,
+        bonus: false,
+        malus: false,
+        occupe: undefined
+    },
+    {
+        numeroCase: 0,
+        positionX: 60,
+        positionY: 40,
+        bonus: false,
+        malus: false,
+        occupe: undefined
+    },
+    {
+        numeroCase: 0,
+        positionX: 60,
+        positionY: 40,
+        bonus: false,
+        malus: false,
+        occupe: undefined
+    },
+    {
+        numeroCase: 0,
+        positionX: 60,
+        positionY: 40,
+        bonus: false,
+        malus: false,
+        occupe: undefined
+    },
+    {
+        numeroCase: 0,
+        positionX: 60,
+        positionY: 40,
+        bonus: false,
+        malus: false,
+        occupe: undefined
+    },
+    {
+        numeroCase: 0,
+        positionX: 60,
+        positionY: 40,
+        bonus: false,
+        malus: false,
+        occupe: undefined
+    }
+]
