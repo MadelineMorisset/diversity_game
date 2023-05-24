d499b2c4ba11690941d104008a83547916bb11c8
VanessaGui <guimardv@gmail.com>
2023-05-24 15:01:51 +0200

    position cases

--- script.js
+++ script.js
@@ -1,208 +1,224 @@
 let plateau = [
     {
         numeroCase: 0,
-        positionX: 60,
-        positionY: 40,
-        bonus: false,
-        malus: false,
-        occupe: undefined
-    },
-    {
-        numeroCase: 1,
-        positionX: 140,
-        positionY: 130,
-        bonus: false,
-        malus: false,
-        occupe: undefined
-    },
-    {
-        numeroCase: 2,
-        positionX: 210,
-        positionY: 240,
-        bonus: false,
-        malus: false,
-        occupe: undefined
-    },
-    {
-        numeroCase: 3,
-        positionX: 170,
-        positionY: 350,
-        bonus: false,
-        malus: false,
-        occupe: undefined
-    },
-    {
-        numeroCase: 4,
-        positionX: 100,
-        positionY: 450,
-        bonus: false,
-        malus: false,
-        occupe: undefined
-    },
-    {
-        numeroCase: 5,
-        positionX: 150,
-        positionY: 560,
-        bonus: false,
-        malus: true,
-        occupe: undefined
-    },
-    {
-        numeroCase: 6,
-        positionX: 250,
-        positionY: 630,
-        bonus: false,
-        malus: false,
-        occupe: undefined
-    },
-    {
-        numeroCase: 7,
-        positionX: 350,
-        positionY: 560,
-        bonus: false,
-        malus: false,
-        occupe: undefined
-    },
-    {
-        numeroCase: 8,
-        positionX: 400,
-        positionY: 450,
-        bonus: false,
-        malus: false,
-        occupe: undefined
-    },
-    {
-        numeroCase: 9,
-        positionX: 400,
-        positionY: 330,
-        bonus: false,
-        malus: false,
-        occupe: undefined
-    },
-    {
-        numeroCase: 10,
-        positionX: 360,
-        positionY: 210,
-        bonus: true,
-        malus: false,
-        occupe: undefined
-    },
-    {
-        numeroCase: 11,
-        positionX: 380,
+        positionX: 220,
         positionY: 90,
         bonus: false,
         malus: false,
         occupe: undefined
     },
     {
-        numeroCase: 0,
-        positionX: 60,
-        positionY: 40,
+        numeroCase: 1,
+        positionX: 35,
+        positionY: 180,
         bonus: false,
         malus: false,
         occupe: undefined
     },
     {
-        numeroCase: 0,
-        positionX: 60,
-        positionY: 40,
+        numeroCase: 2,
+        positionX: 440,
+        positionY: 180,
         bonus: false,
         malus: false,
         occupe: undefined
     },
     {
-        numeroCase: 0,
-        positionX: 60,
-        positionY: 40,
+        numeroCase: 3,
+        positionX: 390,
+        positionY: 400,
         bonus: false,
         malus: false,
         occupe: undefined
     },
     {
-        numeroCase: 0,
-        positionX: 60,
-        positionY: 40,
+        numeroCase: 4,
+        positionX: 310,
+        positionY: 500,
         bonus: false,
         malus: false,
         occupe: undefined
     },
     {
-        numeroCase: 0,
-        positionX: 60,
-        positionY: 40,
+        numeroCase: 5,
+        positionX: 380,
+        positionY: 610,
+        bonus: false,
+        malus: true,
+        occupe: undefined
+    },
+    {
+        numeroCase: 6,
+        positionX: 510,
+        positionY: 680,
         bonus: false,
         malus: false,
         occupe: undefined
     },
     {
-        numeroCase: 0,
-        positionX: 60,
-        positionY: 40,
+        numeroCase: 7,
+        positionX: 630,
+        positionY: 610,
         bonus: false,
         malus: false,
         occupe: undefined
     },
     {
-        numeroCase: 0,
-        positionX: 60,
-        positionY: 40,
+        numeroCase: 8,
+        positionX: 700,
+        positionY: 500,
         bonus: false,
         malus: false,
         occupe: undefined
     },
     {
-        numeroCase: 0,
-        positionX: 60,
-        positionY: 40,
+        numeroCase: 9,
+        positionX: 700,
+        positionY: 380,
         bonus: false,
         malus: false,
         occupe: undefined
     },
     {
-        numeroCase: 0,
-        positionX: 60,
-        positionY: 40,
+        numeroCase: 10,
+        positionX: 660,
+        positionY: 260,
+        bonus: true,
+        malus: false,
+        occupe: undefined
+    },
+    {
+        numeroCase: 11,
+        positionX: 680,
+        positionY: 140,
         bonus: false,
         malus: false,
         occupe: undefined
     },
     {
-        numeroCase: 0,
-        positionX: 60,
-        positionY: 40,
+        numeroCase: 12,
+        positionX: 830,
+        positionY: 90,
         bonus: false,
         malus: false,
         occupe: undefined
     },
     {
-        numeroCase: 0,
-        positionX: 60,
-        positionY: 40,
+        numeroCase: 13,
+        positionX: 960,
+        positionY: 140,
         bonus: false,
         malus: false,
         occupe: undefined
     },
     {
-        numeroCase: 0,
-        positionX: 60,
-        positionY: 40,
+        numeroCase: 14,
+        positionX: 990,
+        positionY: 260,
+        bonus: false,
+        malus: true,
+        occupe: undefined
+    },
+    {
+        numeroCase: 15,
+        positionX: 940,
+        positionY: 380,
         bonus: false,
         malus: false,
         occupe: undefined
     },
     {
-        numeroCase: 0,
-        positionX: 60,
-        positionY: 40,
+        numeroCase: 16,
+        positionX: 940,
+        positionY: 500,
         bonus: false,
         malus: false,
         occupe: undefined
     },
     {
-        numeroCase: 0,
-        positionX: 60,
-        positionY: 40,
+        numeroCase: 17,
+        positionX: 960,
+        positionY: 620,
+        bonus: false,
+        malus: false,
+        occupe: undefined
+    },
+    {
+        numeroCase: 18,
+        positionX: 1090,
+        positionY: 720,
+        bonus: true,
+        malus: false,
+        occupe: undefined
+    },
+    {
+        numeroCase: 19,
+        positionX: 1250,
+        positionY: 650,
+        bonus: false,
+        malus: false,
+        occupe: undefined
+    },
+    {
+        numeroCase: 20,
+        positionX: 1250,
+        positionY: 520,
+        bonus: false,
+        malus: false,
+        occupe: undefined
+    },
+    {
+        numeroCase: 21,
+        positionX: 1250,
+        positionY: 390,
+        bonus: false,
+        malus: false,
+        occupe: undefined
+    },
+    {
+        numeroCase: 22,
+        positionX: 1230,
+        positionY: 260,
+        bonus: false,
+        malus: true,
+        occupe: undefined
+    },
+    {
+        numeroCase: 23,
+        positionX: 1320,
+        positionY: 140,
+        bonus: false,
+        malus: false,
+        occupe: undefined
+    },
+    {
+        numeroCase: 24,
+        positionX: 1470,
+        positionY: 100,
+        bonus: false,
+        malus: false,
+        occupe: undefined
+    },
+    {
+        numeroCase: 25,
+        positionX: 1590,
+        positionY: 180,
+        bonus: true,
+        malus: false,
+        occupe: undefined
+    },
+    {
+        numeroCase: 26,
+        positionX: 1550,
+        positionY: 320,
+        bonus: false,
+        malus: false,
+        occupe: undefined
+    },
+    {
+        numeroCase: 27,
+        positionX: 1490,
+        positionY: 450,
         bonus: false,
         malus: false,
         occupe: undefined
