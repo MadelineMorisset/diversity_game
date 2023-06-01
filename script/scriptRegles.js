e1c737202b35c76c0a60017a6b824f03b86a95f4
Madeline Morisset <12475097+MadelineMorisset@users.noreply.github.com>
2023-06-01 13:35:02 +0200

    Fichier obsolète

--- script/scriptRegles.js
+++
@@ -1,6 +0,0 @@
-let btn = document.getElementById('push');
-    btn.addEventListener('click',afficherRegles);
-function afficherRegles() {
-    let rules =document.getElementById('rules')
-    rules.classList.remove('hidden')
-}
